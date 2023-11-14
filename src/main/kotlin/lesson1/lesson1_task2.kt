949c6d6ac6acc294e8f11ad42e18ff397c7418f6
dzenonce <markin-dmitrij@yandex.ru>
2023-11-14 21:25:11 +0300

    KS-1-2 Имитация логов с сервера

---
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -0,0 +1,19 @@
+package lesson1
+
+fun main() {
+
+    val quantityOrders = 75
+    val thankingPurchaseText = "Благодарим Вас за покупку, ждем вас снова"
+
+    println(quantityOrders)
+    println(thankingPurchaseText)
+
+    var amountWorkers = 2000
+
+//    println(amountOfWorkers)
+
+    amountWorkers = 1999
+
+    println(amountWorkers)
+
+}
