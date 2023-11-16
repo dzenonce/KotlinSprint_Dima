bbdc813e7396e97bb77c3c053959936207c0ab26
dzenonce <markin-dmitrij@yandex.ru>
2023-11-16 13:58:37 +0300

    KS-2-3 Time Arrival, append const

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -1,16 +1,18 @@
 package lesson2
 
+const val sixty = 60
+
 fun main() {
     val departTimeHour = 9
     val departTimeMinute = 39
 
     val travelTimeMinute = 457
 
-    val departTimeHourInMinute = departTimeHour * 60
+    val departTimeHourInMinute = departTimeHour * sixty
 
     val arrivalTimeInMinute = departTimeHourInMinute + departTimeMinute + travelTimeMinute
-    val hourOfArrival = arrivalTimeInMinute / 60
-    val minuteOfArrival = arrivalTimeInMinute % 60
+    val hourOfArrival = arrivalTimeInMinute / sixty
+    val minuteOfArrival = arrivalTimeInMinute % sixty
 
     println("Время прибытия: $hourOfArrival:$minuteOfArrival")
 }
