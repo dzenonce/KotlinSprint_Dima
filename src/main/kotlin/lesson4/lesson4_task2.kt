553a0e53c10d635f79561f8476456a40c647ad29
dzenonce <markin-dmitrij@yandex.ru>
2023-11-17 19:30:27 +0300

    KS-4-2 Cargo weight, capacity

---
+++ src/main/kotlin/lesson4/lesson4_task2.kt
@@ -0,0 +1,18 @@
+package lesson4
+
+const val MINIMUM_WEIGHT = 35
+const val MAXIMUM_WEIGHT = 100
+const val MAXIMUM_CAPACITY = 100
+
+fun main() {
+    val firstCargoKg = 20
+    val firstCargoCapacity = 80
+
+    val secondCargoKg = 50
+    val secondCargoCapacity = 100
+
+    println("""
+        Груз с весом $firstCargoKg кг и объемом $firstCargoCapacity л соответствует категории 'Average': ${ (firstCargoKg >= MINIMUM_WEIGHT) && (firstCargoCapacity < MAXIMUM_CAPACITY) }
+        Груз с весом $secondCargoKg кг и объемом $secondCargoCapacity л соответствует категории 'Average': ${ (secondCargoKg >= MINIMUM_WEIGHT) && (secondCargoCapacity < MAXIMUM_CAPACITY) }
+    """.trimIndent())
+}
