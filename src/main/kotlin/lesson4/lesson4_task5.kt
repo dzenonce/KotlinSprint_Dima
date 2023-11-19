ef511929f76041d77f57617d19c4b721e136773e
dzenonce <markin-dmitrij@yandex.ru>
2023-11-19 22:28:09 +0300

    KS-4-5 Refactoring

--- src/main/kotlin/lesson4/lesson4_task5.kt
+++ src/main/kotlin/lesson4/lesson4_task5.kt
@@ -3,35 +3,35 @@ package lesson4
 fun main() {
 
     println("Есть ли у корабля повреждения (введите true/false): ")
-    val shipDamage = readLine().toBoolean()
+    val isShipDamage = readln().toBoolean()
 
     println("Введите количество экипажа: ")
-    val crewSize = readLine()!!.toInt()
+    val crewSize = readln()?.toInt()
 
     println("Введите количество ящиков провизии: ")
-    val amountProvision = readLine()!!.toInt()
+    val amountProvision = readln()?.toInt()
 
-    println("Введите подходящие ли погодныве условия (true/false): ")
-    val weatherCondition = readLine().toBoolean()
+    println("Введите подходящие ли погодные условия (true/false): ")
+    val isSunnyWeather = readln().toBoolean()
 
 //  Здесь не учитываю погоду, поскольку в штатных условиях корабль может отплыть при любой погоде
-    val departureShip =
-        (shipDamage == DAMAGE_SHIP) && (crewSize in MINIMUM_CREW..MAXIMUM_CREW) && (amountProvision >= MINIMUM_SUPPLY_CRATES)
-    println("Может ли корабль уйти в море: $departureShip")
+    val isShipReady =
+        (isShipDamage == HAS_DAMAGE_SHIP) && (crewSize in MINIMUM_CREW..MAXIMUM_CREW) && (amountProvision!! > MINIMUM_SUPPLY_CRATES)
+    println("Может ли корабль уйти в море: $isShipReady")
 
-    if (departureShip != true) {
-        val departureDamagedShip =
-            (shipDamage != DAMAGE_SHIP) && (crewSize == MAXIMUM_CREW) && (weatherCondition == GOOD_WEATHER) && (amountProvision >= MINIMUM_SUPPLY_CRATES)
-        println("Может ли поврежденный корабль уйти в море: $departureDamagedShip")
+    if (isShipReady != true) {
+        val isDamagedShipReady =
+            (isShipDamage != HAS_DAMAGE_SHIP) && (crewSize == MAXIMUM_CREW) && (isSunnyWeather == IS_SUNNY_WEATHER) && (amountProvision!! >= MINIMUM_SUPPLY_CRATES)
+        println("Может ли поврежденный корабль уйти в море: $isDamagedShipReady")
     }
 
 }
 
-const val DAMAGE_SHIP = false
+const val HAS_DAMAGE_SHIP = false
 
 const val MINIMUM_CREW = 55
 const val MAXIMUM_CREW = 70
 
 const val MINIMUM_SUPPLY_CRATES = 50
 
-const val GOOD_WEATHER = true
+const val IS_SUNNY_WEATHER = true
