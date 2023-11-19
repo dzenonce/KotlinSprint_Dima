dbb12705131cee024f10b052b233ddbe34f07770
dzenonce <markin-dmitrij@yandex.ru>
2023-11-19 21:44:33 +0300

    KS-4-3 renaming

--- src/main/kotlin/lesson4/lesson4_task3.kt
+++ src/main/kotlin/lesson4/lesson4_task3.kt
@@ -1,17 +1,17 @@
 package lesson4
 
 fun main() {
-    val currentWeather = true
-    val conditionTent = true
+    val isSunnyWeather = true
+    val isAwningCovered = true
     val airHumiditi = 20
     val currentSeason = "Зима"
 
-    val favorableCondition = (currentWeather == WEATHER) && (conditionTent == CONDITION_TENT) && (airHumiditi == HUMIDITI) && (currentSeason != SEASON)
+    val favorableCondition = (isSunnyWeather == IS_SUNNY_WEATHER) && (isAwningCovered == IS_AWNING_COVERED) && (airHumiditi == HUMIDITI) && (currentSeason != SEASON)
 
     println(favorableCondition)
 }
 
-const val WEATHER = true
-const val CONDITION_TENT = true
+const val IS_SUNNY_WEATHER = true
+const val IS_AWNING_COVERED = true
 const val HUMIDITI = 20
 const val SEASON = "Зима"
