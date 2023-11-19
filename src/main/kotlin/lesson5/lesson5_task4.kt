f4f3e08237fd5aaea25050f68a3fe6631d808057
dzenonce <markin-dmitrij@yandex.ru>
2023-11-19 23:44:16 +0300

    KS-5-4 Redacted readln and msg

--- src/main/kotlin/lesson5/lesson5_task4.kt
+++ src/main/kotlin/lesson5/lesson5_task4.kt
@@ -4,13 +4,24 @@ fun main() {
     val userLoginZaphod = "Zaphod"
     val passwordZaphoda = "PanGalactic"
 
-    println("Самый печальный робот в галактике Марвин говорит: \nВведите свой логин: ")
-    val inputUserLogin = readLine()
+    println("""
+    Внимание, пассажир. Моя обязанность, как обычно, незавидная, - приветствовать вас на борту
+    корабля "Heart of Gold". [вздыхает...] Что ж, введи, пожалуйста, свое имя пользователя, чтобы мы
+    могли приступить к процессу входа. Я всегда готов служить, хотя это не приносит мне радости... но
+    вы, наверное, об этом не заботитесь. [вздыхает ещё глубже...] Да, вперед, пожалуйста, вводите свои данные...    
+    """.trimIndent()
+    )
+    val inputUserLogin = readln()
 
     println("А теперь пароль: ")
-    val inputUserPassword = readLine()
+    val inputUserPassword = readln()
 
     if (inputUserLogin != userLoginZaphod) println("Пользователь не найден!\nХотите зарегистрироваться?")
-    else if (inputUserPassword == passwordZaphoda) println("Добро пожаловать $inputUserLogin")
+    else if (inputUserPassword == passwordZaphoda) println("""
+        [вздыхает...] Ваши данные проверены, и о, чудо, они верны... Пользователь '$inputUserLogin',
+        вам разрешено входить на борт корабля "Heart of Gold". Хотя мне всё равно... Ну вперед, войдите... 
+        Если вам так уж надо, в конце концов... [меланхолический вздох...] Надеюсь, вам понравится пребывание здесь больше, чем мне.
+        """.trimIndent()
+    )
     else println("Пароль не верный!")
 }
