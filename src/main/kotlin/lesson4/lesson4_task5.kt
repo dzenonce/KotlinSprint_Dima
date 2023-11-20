f751f65003f7c052e2f73320555d8ae40f847ea1
dzenonce <markin-dmitrij@yandex.ru>
2023-11-20 13:10:55 +0300

    KS-4-5 Edited

--- src/main/kotlin/lesson4/lesson4_task5.kt
+++ src/main/kotlin/lesson4/lesson4_task5.kt
@@ -3,26 +3,25 @@ package lesson4
 fun main() {
 
     println("Есть ли у корабля повреждения (введите true/false): ")
-    val isShipDamage = readln().toBoolean()
+    val hasShipDamage = readln().toBoolean()
 
     println("Введите количество экипажа: ")
-    val crewSize = readln()?.toInt()
+    val crewSize = readln().toInt()
 
     println("Введите количество ящиков провизии: ")
-    val amountProvision = readln()?.toInt()
+    val amountProvision = readln().toInt()
 
     println("Введите подходящие ли погодные условия (true/false): ")
     val isSunnyWeather = readln().toBoolean()
 
-//  Здесь не учитываю погоду, поскольку в штатных условиях корабль может отплыть при любой погоде
-    val isShipReady =
-        (isShipDamage == HAS_DAMAGE_SHIP) && (crewSize in MINIMUM_CREW..MAXIMUM_CREW) && (amountProvision!! > MINIMUM_SUPPLY_CRATES)
-    println("Может ли корабль уйти в море: $isShipReady")
+    val isShipReadyNow =
+        (hasShipDamage == HAS_DAMAGE_SHIP) && (crewSize in MINIMUM_CREW..MAXIMUM_CREW) && (amountProvision > MINIMUM_SUPPLY_CRATES)
+    println("Может ли корабль уйти в море: $isShipReadyNow")
 
-    if (isShipReady != true) {
-        val isDamagedShipReady =
-            (isShipDamage != HAS_DAMAGE_SHIP) && (crewSize == MAXIMUM_CREW) && (isSunnyWeather == IS_SUNNY_WEATHER) && (amountProvision!! >= MINIMUM_SUPPLY_CRATES)
-        println("Может ли поврежденный корабль уйти в море: $isDamagedShipReady")
+    if (isShipReadyNow != true) {
+        val isShipAndCrewReady =
+            (hasShipDamage != HAS_DAMAGE_SHIP) && (crewSize == MAXIMUM_CREW) && (isSunnyWeather == IS_SUNNY_WEATHER) && (amountProvision >= MINIMUM_SUPPLY_CRATES)
+        println("Может ли поврежденный корабль уйти в море: $isShipAndCrewReady")
     }
 
 }
