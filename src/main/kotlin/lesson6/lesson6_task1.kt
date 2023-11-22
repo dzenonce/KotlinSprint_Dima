2f7b74d637d22db30e8db95a8879c97dea160422
dzenonce <markin-dmitrij@yandex.ru>
2023-11-22 15:40:39 +0300

    KS-6-1 User Reg redacted

--- src/main/kotlin/lesson6/lesson6_task1.kt
+++ src/main/kotlin/lesson6/lesson6_task1.kt
@@ -2,29 +2,20 @@ package lesson6
 
 fun main() {
 
-    println(
-        """Регистрация нового пользователя!
-    |Введите желаемый логин: """.trimMargin()
-    )
+    println("Регистрация нового пользователя! \nВведите желаемый логин: ")
     val newUserLogin = readln()
 
     println("Введите пароль: ")
     val newUserPassword = readln()
 
     do {
-        println(
-            """Выполните вход в приложение!
-        |Введите ваш логин:
-        """.trimMargin()
-        )
+        println("Выполните вход в приложение! \nВведите ваш логин:")
         val userLogin = readln()
 
         println("Введите ваш пароль: ")
         val userPassword = readln()
 
-        val isCorrectInput = (userLogin == newUserLogin) && (userPassword == newUserPassword)
-
-    } while (!isCorrectInput)
+    } while ((userLogin != newUserLogin) || (userPassword != newUserPassword))
 
     println("Авторизация прошла успешно!")
 
