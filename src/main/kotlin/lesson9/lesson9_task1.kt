0e059c90e73ecf4ebb1f804974dcb48a2abe13ec
dzenonce <markin-dmitrij@yandex.ru>
2023-11-23 23:02:21 +0300

    KS-9-1 Ingredient

---
+++ src/main/kotlin/lesson9/lesson9_task1.kt
@@ -0,0 +1,9 @@
+package lesson9
+
+fun main() {
+    val ingredientList = listOf("Яйцо", "Лук", "Мука", "Зелень")
+
+    println("В рецепте есть следующие ингредиенты: ")
+    ingredientList.forEach() { println(it) }
+
+}
