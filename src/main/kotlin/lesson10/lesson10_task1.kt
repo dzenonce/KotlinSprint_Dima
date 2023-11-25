6e072122fee96012b10d2b28694f276544af1314
dzenonce <markin-dmitrij@yandex.ru>
2023-11-25 14:50:12 +0300

    KS-10-1 Dice Roll

--- src/main/kotlin/lesson10/lesson10_task1.kt
+++ src/main/kotlin/lesson10/lesson10_task1.kt
@@ -1,18 +1,18 @@
 package lesson10
 
 fun main() {
-    val humanStep = throwADice()
-    println("Бросил человек, на костях $humanStep")
 
-    val robotStep = throwADice()
-    println("Бросил робот, на костях $robotStep")
+    val humanDice = rollDice()
+    println("Человек бросает кость, результат $humanDice")
 
-    val gameResult = if (humanStep > robotStep) "Победил человек"
-    else if (humanStep < robotStep) "Победила машина"
-    else "Ничья"
+    val robotDice = rollDice()
+    println("Робот бросает кость, результат $robotDice")
+
+    val gameResult = if (humanDice > robotDice) "Человек победил!"
+    else if (humanDice < robotDice) "Машина победила!"
+    else "Ничья!"
 
     println(gameResult)
 }
-// to:do
-// Сделать на несколько ходов, с записью в лист результатов и по сумме вывод реза
-fun throwADice() : Int = (1..6).random()
+
+fun rollDice() : Int = (1..6).random()
