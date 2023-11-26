3e3a1db622f422403f4e323b83040dff6f34f216
dzenonce <markin-dmitrij@yandex.ru>
2023-11-26 11:45:38 +0300

    KS-8-5 Update Cool

--- src/main/kotlin/lesson8/lesson8_task5.kt
+++ src/main/kotlin/lesson8/lesson8_task5.kt
@@ -2,15 +2,14 @@ package lesson8
 
 fun main() {
     println("Введите количесво ингредиентов в рецепте")
-    val quantityIngredients = readln().toInt()
 
-    var arrayIngredient = Array (quantityIngredients) { "" }
+    var i = 0
 
-    for (items in 0 until quantityIngredients) {
-        println("Введите ингредиент ${items + 1}")
-        val ingredients = readln()
-        arrayIngredient[items] = ingredients
-    }
+    val arrayIngredient =
+        Array(readln().toInt()) {
+            println("Введите ингредиент ${++i}")
+            readln()
+        }
 
     println("В рецепте теперь:")
     arrayIngredient.forEach { ingredients -> println(ingredients) }
