5d7788ffebbdcfb5f8852fbaa9395e4172d733d3
dzenonce <markin-dmitrij@yandex.ru>
2023-11-30 12:00:24 +0300

    KS-11-5 Forum Siro

--- src/main/kotlin/lesson11/lesson11_task5.kt
+++ src/main/kotlin/lesson11/lesson11_task5.kt
@@ -2,9 +2,8 @@ package lesson11
 
 class Forum(
     val memberList: MutableList<ForumMember> = mutableListOf(),
-
+    val forunMessage: MutableList<String> = mutableListOf(),
 ) {
-
 
     fun newForumMember(userName: String): ForumMember {
         var userCount = memberList.size
@@ -19,6 +18,16 @@ class Forum(
 
     }
 
+    fun createNewMessage(user: ForumMember, message: String) {
+        val forum = Forum()
+
+        for (i in 0 .. memberList.size) {
+            if (forum.memberList[i].userId.equals(user.userId)) println("Есть $message")
+            else println("Нет")
+        }
+
+    }
+
 }
 
 class ForumMember(
@@ -30,21 +39,26 @@ class ForumMessage(
     val authorId: Int,
     val message: String,
 
-)
+    )
 
 fun main() {
 
-    val newUser = Forum()
-    println(newUser.memberList.size)
-    val user1 = newUser.newForumMember("mai")
-    val user2 = newUser.newForumMember("ma")
-    val user3 = newUser.newForumMember("mami")
+    val forum = Forum()
+    println(forum.memberList.size)
+    val user1 = forum.newForumMember("mai")
+    val user2 = forum.newForumMember("ma")
+    val user3 = forum.newForumMember("mami")
 
 
     println("${user1.userId} ,${user1.userName}")
     println("${user2.userId} ,${user2.userName}")
     println("${user3.userId} ,${user3.userName}")
 
-    println(newUser.memberList[0].userName)
+    println(forum.memberList[0].userName)
+
+    val id = 1
+    forum.memberList.forEach() { println(it) }
+
+    forum.createNewMessage(user1, "Привет")
 
 }
