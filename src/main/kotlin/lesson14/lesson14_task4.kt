ab4ad202ce10a50cd6f4b37bfd7e274f44b4192e
dzenonce <markin-dmitrij@yandex.ru>
2023-12-04 14:23:55 +0300

    KS-14-4 Satellite

---
+++ src/main/kotlin/lesson14/lesson14_task4.kt
@@ -0,0 +1,49 @@
+package lesson14
+
+open class CelestialBody(
+    val hasSatellite: Boolean = false,
+)
+
+class Planet(
+    val name: String,
+    val hasAtmosphere: Boolean,
+    val isPlanetarySuitability: Boolean,
+    hasSatellite: Boolean,
+    val listSatellite: MutableList<Satellite> = mutableListOf(),
+) : CelestialBody(hasSatellite)
+
+class Satellite(
+    val name: String,
+    val hasAtmosphere: Boolean,
+    val isPlanetarySuitability: Boolean,
+    hasSatellite: Boolean = false,
+) : CelestialBody(hasSatellite)
+
+fun main() {
+
+    val planet = Planet(
+        name = "B-253",
+        hasAtmosphere = true,
+        isPlanetarySuitability = true,
+        hasSatellite = true,
+    )
+
+    val satellite1 = Satellite(
+        name = "S-B-253.1",
+        hasAtmosphere = false,
+        isPlanetarySuitability = true,
+    )
+
+    val satellite2 = Satellite(
+        name = "S-B-253.2",
+        hasAtmosphere = false,
+        isPlanetarySuitability = false,
+    )
+
+    planet.listSatellite.add(satellite1)
+    planet.listSatellite.add(satellite2)
+
+    println("У планеты ${planet.name} есть спутники:")
+    planet.listSatellite.forEach() { println(it.name) }
+
+}
