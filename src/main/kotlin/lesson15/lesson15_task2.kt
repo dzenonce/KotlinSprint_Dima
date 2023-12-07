4aea806bb99e7ee4c35e09d577dc54109e08b87c
dzenonce <markin-dmitrij@yandex.ru>
2023-12-07 23:29:46 +0300

    KS-15-2 Data Weather When use

---
+++ src/main/kotlin/lesson15/lesson15_task2.kt
@@ -0,0 +1,38 @@
+package lesson15
+
+abstract class WeatherStationStats
+
+class Temperature(
+    val morningTemperature: Double,
+    val dayTemperature: Double,
+    val nightTemperature: Double,
+) : WeatherStationStats()
+
+class PrecipitationAmount(
+    val precipitation: Double,
+) : WeatherStationStats()
+
+class WeatherServer {
+
+    fun sendWeatherData(weatherData: WeatherStationStats) {
+        when (weatherData) {
+            is Temperature -> println("Отправляются данные о температуре")
+            is PrecipitationAmount -> println("Отправляются данные о влажности")
+            else -> println("Тип данных не определен")
+        }
+    }
+}
+
+fun main() {
+
+    val temperature = Temperature(
+        1.1,
+        3.2,
+        5.7,
+    )
+
+    val server = WeatherServer()
+
+    server.sendWeatherData(temperature)
+
+}
