98ef0fec920d25e525d84701ca1aa463271e72a9
dzenonce <markin-dmitrij@yandex.ru>
2023-12-15 14:08:54 +0300

    KS-20-2 Health Personage

---
+++ src/main/kotlin/lesson20/lesson20_task2.kt
@@ -0,0 +1,38 @@
+package lesson20
+
+import kotlin.math.max
+
+class Personage(
+    val name: String,
+    var currentHealth: Int,
+    val maxHealth: Int,
+) {
+
+    fun printPersonageInfo() {
+        println("""
+            Имя: $name
+            Текущее здоровье: $currentHealth
+        """.trimIndent())
+    }
+
+}
+
+fun main() {
+
+    val healingPotion: (Personage) -> Unit = { pers: Personage ->
+        pers.currentHealth = pers.maxHealth
+    }
+
+    val pers = Personage(
+        name = "LordOne",
+        currentHealth = 10,
+        maxHealth = 100,
+    )
+
+    pers.printPersonageInfo()
+
+    healingPotion(pers)
+
+    pers.printPersonageInfo()
+
+}
